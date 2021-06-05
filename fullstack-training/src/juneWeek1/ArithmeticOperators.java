929e0cd027b7cf115c9334e2b21c3a7e72c8684a
K Udaya Kumar <64793634+K-Udaya@users.noreply.github.com>
2021-06-05 18:52:01 +0530

    Second Commit

---
+++ fullstack-training/src/juneWeek1/ArithmeticOperators.java
@@ -0,0 +1,19 @@
+package juneWeek1;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(args.length);
+		
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		
+		System.out.println("Add" + " " + (a+b));
+		System.out.println("Sub" + "" + (a-b));
+		System.out.println("Mul" + " " + a*b);
+
+	}
+
+}
