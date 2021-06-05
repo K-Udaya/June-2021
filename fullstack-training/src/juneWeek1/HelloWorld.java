f2a6b6e4934ab63335caa88f01a14a93e7f2f3ba
K Udaya Kumar <64793634+K-Udaya@users.noreply.github.com>
2021-06-05 13:05:15 +0530

    IOS Bug Fixed!

---
+++ fullstack-training/src/juneWeek1/HelloWorld.java
@@ -0,0 +1,12 @@
+package juneWeek1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Wrold");
+
+	}
+
+}
